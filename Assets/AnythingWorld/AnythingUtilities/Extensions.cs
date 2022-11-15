a4dcf8604f35fb5cf747d020ee85f31042085f51
salgueronicSolvo <nicolas.salguero@solvoglobal.com>
2022-11-15 20:01:51 -0300

    removed anything world folders

--- Assets/AnythingWorld/AnythingUtilities/Extensions.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-using System.Linq;
-
-public static class EnumExtension
-{
-    public static IEnumerable<(T item, int index)> WithIndex<T>(this IEnumerable<T> self)
-       => self.Select((item, index) => (item, index));
-}
-
